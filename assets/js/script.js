36f7596e65071b61928d4cd4b06c7b8bfecd8e63
vanesaivch <vanesach96@abv.bg>
2026-09-02 22:25:51 +0100

    created main functions in js

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+let cards = document.querySelectorAll('.card');
+
+function shuffleCards() {}
+
+function flipCard() {}
+
+function checkForMatch() {}
+
+function resetCards() {}
+
+function resetGame() {}
+
+function checkGameComplete() {}
+
