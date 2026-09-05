e08d81068f4daa1054b9b0e9292371281353253e
vanesaivch <vanesach96@abv.bg>
2026-09-05 15:12:38 +0100

    added a current Target back

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,14 @@
 const cardContainer = Array.from(document.querySelectorAll('.card-container'));
 
-let firstCard = null;
-let secondCard = null; 
-let lockBoard = false;
+
 cardContainer.forEach((cardContainer) => {
     cardContainer.addEventListener('click', flipCard);
 });
 
 function flipCard(event) {
-
+       event.currentTarget.classList.toggle('flipped');
 }
+
 function checkForMatch(cardContainer) {
     let firstCard = document.querySelector('.inner-card img');
     let secondCard = document.querySelector('.inner-card img');
