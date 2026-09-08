1d59f7d2c7e2e60be7b19844450d923fb2d59099
vanesaivch <vanesach96@abv.bg>
2026-09-08 21:37:20 +0100

    created flipCard function

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,11 +6,24 @@ let lockBoard = false;
 
 cardContainer.forEach((cardContainer) => {
     cardContainer.addEventListener('click', flipCard);
+
 });
 
 function flipCard(event) {
+    
+    if (lockBoard) return;
        event.currentTarget.classList.toggle('flipped');
+
+    if (!firstCard) {
+        firstCard = event.currentTarget;
+    } else if (event.currentTarget !== firstCard) {
+        secondCard = event.currentTarget;
+        checkForMatch(firstCard, secondCard);
+        firstCard = null;
+        secondCard = null;
+    }
 }
+
 
 function checkForMatch(firstCard, secondCard) {
     if ( firstCard.dataset.type === secondCard.dataset.type ) {
